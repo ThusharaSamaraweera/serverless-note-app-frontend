c53cdedca39aee21eacc7150d868cba5c4c84909
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-09-11 09:37:28 +0530

    add signup, login link into navbar and not found page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
-import Navbar from "react-bootstrap/Navbar";
+import {Navbar, Nav} from "react-bootstrap";
 import Routes from "./routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          Scratch
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Scratch
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
-      <Routes/>
+      <Routes />
     </div>
   );
 }
