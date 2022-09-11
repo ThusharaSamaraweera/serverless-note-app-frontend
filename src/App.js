9d2d1087642b41041461fce5193cddb4a69c2732
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-09-11 09:29:22 +0530

    add boostrap and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Navbar from "react-bootstrap/Navbar";
+import Routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <Navbar.Brand className="font-weight-bold text-muted">
+          Scratch
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar>
+      <Routes/>
     </div>
   );
 }
